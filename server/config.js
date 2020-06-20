14ee78a7516e00d66e2faff8801f3cd148c83801
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2020-06-20 12:04:25 +0530

    Changed .env and config files after creating tables in heroku

--- server/config.js
+++ server/config.js
@@ -2,11 +2,8 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 })
 
 module.exports = pool
